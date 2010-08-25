fc8ee9886ab3764e439efcf1adca1f20f63c1d22
Stas SUȘCOV <stas@net.utcluj.ro>
2010-08-25 13:42:18 +0300

    Closes #2, per group option

--- groups/bpsp-groups.class.php
+++ groups/bpsp-groups.class.php
@@ -32,6 +32,8 @@ class BPSP_Groups {
     function BPSP_Groups() {
         add_action( 'wp', array( &$this, 'set_nav' ), 2 );
 	add_filter( 'groups_get_groups', array( &$this, 'extend_search' ), 10, 2 );
+	add_action( 'groups_admin_tabs', array( &$this, 'group_admin_tab' ), 10, 2 );
+	add_action( 'wp', array( &$this, 'group_admin_screen' ), 4 );
     }
     
     /**
@@ -47,6 +49,32 @@ class BPSP_Groups {
     }
     
     /**
+     * courseware_status( $group_id = null )
+     *
+     * Checks if courseware is enabled for current group
+     * @param Int $group_id, the id of the group, default is null and global $bp will be used
+     * @return Bool, true on success, and false on failure.
+     */
+    function courseware_status( $group_id = null ) {
+	if( !$group_id ) {
+	    global $bp;
+	    $group_id = $bp->groups->current_group->id;
+	}
+	
+	$global_status = get_option( 'bpsp_global_status' );
+	$group_status = groups_get_groupmeta( $group_id, 'courseware' );
+	
+	if( 'true' == $group_status )
+	    return true;
+	elseif ( 'false' == $group_status )
+	    return false;
+	elseif( !empty( $global_status ) )
+	    return true;
+	else
+	    return false;
+    }
+    
+    /**
      * set_nav()
      *
      * Sets up the component navigation
@@ -54,6 +82,9 @@ class BPSP_Groups {
     function set_nav() {
         global $bp;
         
+	if( !$this->courseware_status( $bp->groups->current_group->id ) )
+	    return;
+	
 	if ( $group_id = BP_Groups_Group::group_exists($bp->current_action) ) {
 		$bp->is_single_item = true;
 		$bp->groups->current_group = &new BP_Groups_Group( $group_id );
@@ -85,7 +116,10 @@ class BPSP_Groups {
     function screen_handler() {
         global $bp;
 	
-        if ( $bp->current_component == $bp->groups->slug && $bp->current_action == $bp->courseware->slug ) {
+        if( !$this->courseware_status( $bp->groups->current_group->id ) )
+	    return;
+	
+	if ( $bp->current_component == $bp->groups->slug && $bp->current_action == $bp->courseware->slug ) {
 	    $this->current_nav_option =  $this->nav_options[__( 'Home', 'bpsp' )];
 	    
 	    if( $bp->action_variables[0] )
@@ -201,6 +235,68 @@ class BPSP_Groups {
 	    }
 	}
 	return $groups;
+    }
+    
+    /**
+     * group_admin_tab( $current_tab, $group_slug )
+     *
+     * Hooks into groups_admin_tabs, and adds the courseware options tab
+     */
+    function group_admin_tab( $current_tab, $group_slug ) {
+	global $bp;
+	
+	$tab_content = '<li ';
+	if ( 'courseware' == $current_tab )
+	    $tab_content .= 'class="current"';
+	
+	$tab_content .= '><a href="' . $bp->root_domain . '/' . $bp->groups->slug;
+	$tab_content .= '/' . $group_slug . '/admin/courseware">';
+	$tab_content .= __( 'Courseware', 'buddypress' ) . '</a></li>';
+	
+	echo $tab_content;
+    }
+    
+    /**
+     * group_admin_screen()
+     *
+     * Hooks into wp, adds a new screen to group Admin screens
+     */
+    function group_admin_screen() {
+	global $bp;
+	
+	if ( $bp->current_component == $bp->groups->slug && 'courseware' == $bp->action_variables[0] ) {
+	    if ( $bp->is_item_admin || $bp->is_item_mod  ) {
+		add_action( 'bp_before_group_admin_content', array( &$this, 'group_admin_content' ) );
+		bp_core_load_template( apply_filters( 'groups_template_group_admin', 'groups/single/home' ) );
+	    }
+	}
+    }
+    
+    /**
+     * group_admin_content()
+     *
+     * Hooks into bp_before_group_admin_content(), adds Courseware group options
+     */
+    function group_admin_content() {
+	global $bp;
+	$nonce_name = 'courseware_group_option';
+	
+	if ( isset( $_POST['save'] ) && wp_verify_nonce( $_POST['_wpnonce'], $nonce_name ) ) {
+	    if( isset( $_POST['group_courseware_status'] ) && !empty( $_POST['group_courseware_status'] ) ) {
+		$post_value = sanitize_key( $_POST['group_courseware_status'] );
+		
+		if( groups_update_groupmeta( $bp->groups->current_group->id, 'courseware', $post_value ) )
+		    $vars['message'] = __( 'Group Courseware settings were successfully updated.', 'bpsp' );
+		else
+		    $vars['error'] = __( 'Group Courseware settings were not updated.', 'bpsp' );
+	    }
+	}
+	
+	$vars['name'] = '_group_admin_screen';
+	$vars['form_action'] = $bp->root_domain . '/' . $bp->groups->slug . '/' . $group_slug . '/admin/courseware';
+	$vars['form_nonce'] = wp_nonce_field( $nonce_name, '_wpnonce', true, false );
+	$vars['current_status'] = groups_get_groupmeta( $bp->groups->current_group->id, 'courseware' );
+	$this->load_template( $vars );
     }
 }
 ?>
