59f071921d3bded6c086f63a7724bd3d7a160f95
Stas SUȘCOV <stas@net.utcluj.ro>
2011-07-01 02:12:26 +0300

    Got rid of raquos

--- groups/templates/_no_course_group_header.php
+++ groups/templates/_no_course_group_header.php
@@ -2,7 +2,7 @@
     <p>
         <?php _e( 'You can manage this group Courseware.', 'bpsp' ); ?>
         <a href="<?php echo $init_course_link ?>" id="courseware-init-course" class="action" >
-            <?php _e( 'Edit Course &raquo;', 'bpsp' ); ?>
+            <?php _e( 'Edit Course', 'bpsp' ); ?>
         </a>
     </p>
 </div>
