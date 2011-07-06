ae6ec04b0b4f37bacbd2949c595ea3ce8ab19a94
Stas SUȘCOV <stas@net.utcluj.ro>
2011-07-06 07:01:34 +0300

    New stylin' for lectures

--- groups/templates/_lectures.php
+++ groups/templates/_lectures.php
@@ -1,11 +1,14 @@
 <?php if ( !empty( $lectures ) ): ?>
     <div id="lectures-tree">
-        <input type="text" id="lectures-tree-search-text" class="hide-if-no-js" />
-        <input type="submit" id="lectures-tree-search-submit" class="hide-if-no-js" value="<?php _e( "Search Lectures", "bpsp" ); ?>" />
-        <a href="#lectures-tree-toggle" id="lectures-tree-toggle" class="button" rel="collapse">
-            <span class="collapse-all"><?php _e( "Collapse All", "bpsp" ); ?></span>
-            <span class="expand-all" style="display: none;"><?php _e( "Expand All", "bpsp" ); ?></span>
-        </a>
+        <h4 class="lectures">
+            <span class="icon"></span>
+            <?php _e( 'Course lectures', 'bpsp'); ?>
+        </h4>
+        <div id="lectures-search">
+            <input type="text" id="lectures-tree-search-text" class="hide-if-no-js" />
+            <input type="submit" id="lectures-tree-search-submit" class="hide-if-no-js" value="<?php _e( "Search Lectures", "bpsp" ); ?>" />
+            <a href="#lectures-tree-toggle" id="lectures-tree-toggle" class="button" rel="collapse" name="Expand All"><?php _e( "Collapse All", "bpsp" ); ?></a>
+        </div>
         
         <div id="lectures-tree-container"></div>
         <ul id="lectures-tree-data" class="no-js">
