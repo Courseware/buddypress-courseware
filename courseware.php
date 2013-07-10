17e9464b8f377364a445b311d944e0ddaf5f8988
Ian Dunn <ian@iandunn.name>
2013-07-10 16:31:41 -0700

    Avoid loading the plugin when BuddyPress is being activated
    Either BPSP will call a function that's undefined (because the mock isn't setup), or BuddyPress will try to redeclare a mocked function.

--- courseware.php
+++ courseware.php
@@ -11,139 +11,155 @@ Tested up to: WordPress 3.5 / BuddyPress 1.6
 Author URI: https://github.com/Courseware/buddypress-courseware/contributors
 */
 
-define( 'BPSP_VERSION', '0.9.6' );
-define( 'BPSP_DEBUG', (bool) WP_DEBUG ); // This will allow you to see post types in wp-admin
-define( 'BPSP_PLUGIN_DIR', dirname( __FILE__ ) );
-define( 'BPSP_WEB_URI', WP_PLUGIN_URL . '/' . basename( BPSP_PLUGIN_DIR ) );
-define( 'BPSP_PLUGIN_FILE', basename( BPSP_PLUGIN_DIR ) . '/' . basename( __FILE__ ) );
-
-/* Load the components */
-require_once BPSP_PLUGIN_DIR . '/wordpress/wordpress.class.php';
-require_once BPSP_PLUGIN_DIR . '/mock-buddypress/mock-buddypress.class.php';
-require_once BPSP_PLUGIN_DIR . '/roles/roles.class.php';
-require_once BPSP_PLUGIN_DIR . '/courses/courses.class.php';
-/*require_once BPSP_PLUGIN_DIR . '/lectures/lectures.class.php';
-require_once BPSP_PLUGIN_DIR . '/assignments/assignments.class.php';
-require_once BPSP_PLUGIN_DIR . '/responses/responses.class.php';
-require_once BPSP_PLUGIN_DIR . '/gradebook/gradebook.class.php';
-require_once BPSP_PLUGIN_DIR . '/bibliography/bibliography.class.php';
-require_once BPSP_PLUGIN_DIR . '/bibliography/webapis.class.php';
-require_once BPSP_PLUGIN_DIR . '/schedules/schedules.class.php';*/
-require_once BPSP_PLUGIN_DIR . '/groups/groups.class.php';
-/*require_once BPSP_PLUGIN_DIR . '/dashboards/dashboards.class.php';
-require_once BPSP_PLUGIN_DIR . '/static/static.class.php';
-require_once BPSP_PLUGIN_DIR . '/activity/activity.class.php';
-require_once BPSP_PLUGIN_DIR . '/notifications/notifications.class.php';
-*/
-
-/**
- * i18n
+/*
+ * Avoid loading the plugin when BuddyPress is being activated, because there's no way to avoid fatal errors.
+ * Either BPSP will call a function that's undefined (because the mock isn't setup), or BuddyPress will try to redeclare a mocked function.
  */
-function bpsp_textdomain() {
-    load_plugin_textdomain( 'bpsp', false, basename( BPSP_PLUGIN_DIR ) . '/languages' );
-}
-add_action( 'init', 'bpsp_textdomain' );
+if ( ! bpsp_activating_buddypress() ) {
+	define( 'BPSP_VERSION', '0.9.6' );
+	define( 'BPSP_DEBUG', (bool) WP_DEBUG ); // This will allow you to see post types in wp-admin
+	define( 'BPSP_PLUGIN_DIR', dirname( __FILE__ ) );
+	define( 'BPSP_WEB_URI', WP_PLUGIN_URL . '/' . basename( BPSP_PLUGIN_DIR ) );
+	define( 'BPSP_PLUGIN_FILE', basename( BPSP_PLUGIN_DIR ) . '/' . basename( __FILE__ ) );
 
-/**
- * Register post types and taxonomies
- */
-function bpsp_registration() {
-    BPSP_Courses::register_post_types();
-	/*
-    BPSP_Lectures::register_post_types();
-    BPSP_Assignments::register_post_types();
-    BPSP_Responses::register_post_types();
-    BPSP_Gradebook::register_post_types();
-    BPSP_Bibliography::register_post_types();
-    BPSP_Schedules::register_post_types();
+	/* Load the components */
+	require_once BPSP_PLUGIN_DIR . '/wordpress/wordpress.class.php';
+	require_once BPSP_PLUGIN_DIR . '/mock-buddypress/mock-buddypress.class.php';
+	require_once BPSP_PLUGIN_DIR . '/roles/roles.class.php';
+	require_once BPSP_PLUGIN_DIR . '/courses/courses.class.php';
+	/*require_once BPSP_PLUGIN_DIR . '/lectures/lectures.class.php';
+	require_once BPSP_PLUGIN_DIR . '/assignments/assignments.class.php';
+	require_once BPSP_PLUGIN_DIR . '/responses/responses.class.php';
+	require_once BPSP_PLUGIN_DIR . '/gradebook/gradebook.class.php';
+	require_once BPSP_PLUGIN_DIR . '/bibliography/bibliography.class.php';
+	require_once BPSP_PLUGIN_DIR . '/bibliography/webapis.class.php';
+	require_once BPSP_PLUGIN_DIR . '/schedules/schedules.class.php';*/
+	require_once BPSP_PLUGIN_DIR . '/groups/groups.class.php';
+	/*require_once BPSP_PLUGIN_DIR . '/dashboards/dashboards.class.php';
+	require_once BPSP_PLUGIN_DIR . '/static/static.class.php';
+	require_once BPSP_PLUGIN_DIR . '/activity/activity.class.php';
+	require_once BPSP_PLUGIN_DIR . '/notifications/notifications.class.php';
 	*/
-}
-add_action( 'init', 'bpsp_registration' );
 
-/**
- * On plugins load
- */
-function bpsp_on_plugins_load() {
-    BPSP_Groups::activate_component();
-}
-add_action( 'plugins_loaded', 'bpsp_on_plugins_load', 5 );
+	/**
+	 * i18n
+	 */
+	function bpsp_textdomain() {
+		load_plugin_textdomain( 'bpsp', false, basename( BPSP_PLUGIN_DIR ) . '/languages' );
+	}
+	add_action( 'init', 'bpsp_textdomain' );
 
-/* Initiate the componenets */
-function bpsp_init() {
-    new BPSP_WordPress();
-    new BPSP_Roles();
-    new BPSP_Groups();
-    new BPSP_Courses();
-	/*new BPSP_Lectures();
-    new BPSP_Assignments();
-    new BPSP_Responses();
-    new BPSP_Gradebook();
-    new BPSP_Bibliography();
-    new BPSP_Schedules();
-    new BPSP_Dashboards();
-    new BPSP_Static();
-    new BPSP_Activity();
-    new BPSP_Notifications();
-    */
-}
-add_action( 'init', 'bpsp_init', 6 );
+	/**
+	 * Register post types and taxonomies
+	 */
+	function bpsp_registration() {
+		BPSP_Courses::register_post_types();
+		/*
+		BPSP_Lectures::register_post_types();
+		BPSP_Assignments::register_post_types();
+		BPSP_Responses::register_post_types();
+		BPSP_Gradebook::register_post_types();
+		BPSP_Bibliography::register_post_types();
+		BPSP_Schedules::register_post_types();
+		*/
+	}
+	add_action( 'init', 'bpsp_registration' );
 
-/**
- * bpsp_check()
- * Will check for Courseware dependencies and active components
- *
- * @return True on errors
- * @uses `admin_notices`
- */
-function bpsp_check() {
-    $messages = array();
+	/**
+	 * On plugins load
+	 */
+	function bpsp_on_plugins_load() {
+		BPSP_Groups::activate_component();
+	}
+	add_action( 'plugins_loaded', 'bpsp_on_plugins_load', 5 );
 
-	if ( apply_filters( 'bpsp_require_buddypress', true ) ) {
-		if ( defined( 'BP_VERSION' ) ) {
-			foreach( array( 'groups', 'activity', 'xprofile', 'forums', 'messages' ) as $c )
-				if( !bp_is_active( $c ) )
-					$messages[] = sprintf(
-						__( 'BuddyPress Courseware dependency error: <a href="%1$s">%2$s has to be activated</a>!', 'bpsp' ),
-						admin_url( 'admin.php?page=bp-general-settings' ),
-						$c
-					);
-		} else {
-			$messages[] = sprintf(
-				__( 'BuddyPress Courseware dependency error: Please <a href="%1$s">install BuddyPress</a>!', 'bpsp' ),
-				admin_url( 'plugins.php' )
-			);
+	/* Initiate the componenets */
+	function bpsp_init() {
+		new BPSP_WordPress();
+		new BPSP_Roles();
+		new BPSP_Groups();
+		new BPSP_Courses();
+		/*new BPSP_Lectures();
+		new BPSP_Assignments();
+		new BPSP_Responses();
+		new BPSP_Gradebook();
+		new BPSP_Bibliography();
+		new BPSP_Schedules();
+		new BPSP_Dashboards();
+		new BPSP_Static();
+		new BPSP_Activity();
+		new BPSP_Notifications();
+		*/
+	}
+	add_action( 'init', 'bpsp_init', 6 );
+
+	/**
+	 * bpsp_check()
+	 * Will check for Courseware dependencies and active components
+	 *
+	 * @return True on errors
+	 * @uses `admin_notices`
+	 */
+	function bpsp_check() {
+		$messages = array();
+
+		if ( apply_filters( 'bpsp_require_buddypress', true ) ) {
+			if ( defined( 'BP_VERSION' ) ) {
+				foreach( array( 'groups', 'activity', 'xprofile', 'forums', 'messages' ) as $c )
+					if( !bp_is_active( $c ) )
+						$messages[] = sprintf(
+							__( 'BuddyPress Courseware dependency error: <a href="%1$s">%2$s has to be activated</a>!', 'bpsp' ),
+							admin_url( 'admin.php?page=bp-general-settings' ),
+							$c
+						);
+			} else {
+				$messages[] = sprintf(
+					__( 'BuddyPress Courseware dependency error: Please <a href="%1$s">install BuddyPress</a>!', 'bpsp' ),
+					admin_url( 'plugins.php' )
+				);
+			}
+		}	// @todo else make sure that bbpress is enabled?
+
+		if( !empty( $messages ) ) {
+			echo '<div id="message" class="error fade">';
+				foreach ( $messages as $m )
+					echo "<p>{$m}</p>";
+			echo '</div>';
+			return false;
 		}
-	}	// @todo else make sure that bbpress is enabled?
 
-	if( !empty( $messages ) ) {
-		echo '<div id="message" class="error fade">';
-			foreach ( $messages as $m )
-				echo "<p>{$m}</p>";
-		echo '</div>';
-		return false;
+		return true;
 	}
 
-    return true;
+	/* Activate the components */
+	function bpsp_activation() {
+		if( !bpsp_check() )
+			exit(1);
+		BPSP_Roles::register_profile_fields();
+
+		bpsp_registration();
+		flush_rewrite_rules();
+	}
+	register_activation_hook( BPSP_PLUGIN_FILE, 'bpsp_activation' );
+
+	/* Deactivate the components */
+	function bpsp_deactivation() {
+		flush_rewrite_rules();
+	}
+	register_deactivation_hook( BPSP_PLUGIN_FILE, 'bpsp_activation' );
+
+	// @todo uncomment other modules one by one to get them working. test from start by deacrtivaitn/reactivating plugin
+		// maybe some classes like activity will only be loaded if bp is active b/c they're completely tied to it
 }
 
-/* Activate the components */
-function bpsp_activation() {
-    if( !bpsp_check() )
-        exit(1);
-    BPSP_Roles::register_profile_fields();
+function bpsp_activating_buddypress() {
+	$activating_buddypress = false;
 
-	bpsp_registration();
-	flush_rewrite_rules();
+	if ( 'plugins.php' == $pagenow && isset( $_GET['action'], $_GET['plugin'] ) && 'activate' == $_GET['action'] && 'buddypress/bp-loader.php' == $_GET['plugin'] ) {
+		$activating_buddypress = true;
+	}
+
+	return $activating_buddypress;
 }
-register_activation_hook( BPSP_PLUGIN_FILE, 'bpsp_activation' );
-
-/* Deactivate the components */
-function bpsp_deactivation() {
-	flush_rewrite_rules();
-}
-register_deactivation_hook( BPSP_PLUGIN_FILE, 'bpsp_activation' );
-
-// @todo uncomment other modules one by one to get them working. test from start by deacrtivaitn/reactivating plugin
-	// maybe some classes like activity will only be loaded if bp is active b/c they're completely tied to it
 
 ?>
